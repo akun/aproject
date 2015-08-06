e631e5feadce925efec18618f31f96e9740211c6
akun <6awkun@gmail.com>
2015-08-06 13:24:21 +0800

    0.0.4: update code style

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
 
-
 def hello_world():
     return 'Hello World!'
-
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/test_{{cookiecutter.repo_name}}.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from unittest import TestCase
 
 from {{ cookiecutter.repo_name }}.main import hello_world
@@ -10,4 +9,3 @@ class HelloWordTestCase(TestCase):
 
     def test_hello_world(self):
         self.assertEqual('Hello World!', hello_world())
-
