b476c0ccd082d1c1635a84750b67b5cad617fc33
akun <6awkun@gmail.com>
2015-08-06 13:02:58 +0800

    0.0.4: change to new style requirements
    * update libs
    * change libs
    * update `make install`, `make lint`
    * remove `make lint` from `make all`

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -20,8 +20,24 @@ setup(
     author_email='{{ cookiecutter.email }}',
     license='MIT License',
     url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    package_dir={'': '{{ cookiecutter.repo_name }}'},
-    packages=find_packages('{{ cookiecutter.repo_name }}'),
-    install_requires=get_reqs(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'six>=1.9.0',
+    ],
+    extras_require={
+        'dev': [
+            'prospector>=0.10.2',
+        ],
+        'test': [
+            'coverage>=3.7.1',
+            'coveralls>=0.5',
+            'nose>=1.3.7',
+            'python-coveralls>=2.5.0',
+        ],
+        'docs': [
+            'Sphinx>=1.3.1',
+        ],
+    },
     test_suite='nose.collector',
 )
